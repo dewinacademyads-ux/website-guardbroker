54007d13a2bb38a0be2765c948f5110cd6e0ac88
Junior Araujo <dewinacademy.ads@gmail.com>
2025-11-13 20:04:32 -0300

    updatE

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
-const basePath = isProd ? `/venus-nextjs` : "";
-
 const nextConfig = {
-  output: "export",
-  basePath,
-  assetPrefix: basePath,
+  output: 'export',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
   env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
+    NEXT_PUBLIC_BASE_PATH: '',
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
